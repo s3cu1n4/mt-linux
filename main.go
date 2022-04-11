e6d5de9651a0fb15d9431f5c45885dc26372f1dd
s3cu1n4 <erwin.lic@hotmail.com>
2022-04-11 19:06:12 +0800

    update

--- main.go
+++ main.go
@@ -1,19 +1,65 @@
 package main
 
-/*
-#include "func.h"
-*/
-import "C"
 import (
+	"fmt"
 	"mt-linux/common"
 	"os"
+	"sync"
+	"syscall"
 
 	"github.com/s3cu1n4/logs"
 )
 
-func Startsysmem() {
-	C.Getsysmem()
+func readhookdata() {
+	fd, err := syscall.Open("/proc/elkeid-endpoint", syscall.O_RDONLY, 0)
+	if err != nil {
+		logs.Error("Failed on open elkeid file: ", err)
+	}
+	defer syscall.Close(fd)
 
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	dataChan := make(chan []byte, 100)
+	go func() {
+		wg.Done()
+		for {
+			data := make([]byte, 65536)
+			s := 0
+			n, err := syscall.Read(fd, data)
+			if err != nil {
+				logs.Error("read fd error", err.Error())
+			}
+			for i := 0; i <= n; i++ {
+				if i == n || data[i] == 0x17 {
+					data[i] = 0
+					if i > s {
+						// fmt.Println(string(data[:i]))
+						dataChan <- data
+					}
+					s = i + i
+				} else if data[i] == 0x1e {
+					data[i] = ','
+				}
+			}
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for {
+			data, ok := <-dataChan
+			if !ok {
+				return
+			} else {
+				fmt.Println(string(data))
+			}
+
+			fmt.Println("asdfasdfasfd")
+		}
+	}()
+	wg.Wait()
 }
 
 func main() {
@@ -29,6 +75,6 @@ func main() {
 			os.Exit(-1)
 		}
 	}
+	readhookdata()
 
-	Startsysmem()
 }
