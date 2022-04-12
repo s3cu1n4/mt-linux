816ad598f6d8d3d39c2d4c336c44bacd5bbe1291
s3cu1n4 <erwin.lic@hotmail.com>
2022-04-12 11:31:54 +0800

    udpate

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func readhookdata() {
 			s := 0
 			n, err := syscall.Read(fd, data)
 			if err != nil {
-				logs.Error("read fd error", err.Error())
+				// logs.Error("read fd error", err.Error())
 				return
 			}
 			for i := 0; i <= n; i++ {
